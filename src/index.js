2a6ccb7effd3266a488143b356b735b22a01fc84
Trillzdboy <chukwudubem7@gmail.com>
2022-04-07 15:04:38 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,9 +5,4 @@ import './index.css';
 import App from './App';
 import { AuthContextProvider } from './Store/auth-context';
 
-ReactDOM.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<AuthContextProvider><App /></AuthContextProvider>, document.getElementById('root'));
